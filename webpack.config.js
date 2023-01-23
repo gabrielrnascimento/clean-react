a37dbec8758faee9ba9f0f036c0588cd2e818f8e
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-01-23 20:20:25 -0300

    chore: config jest script and webpack

--- webpack.config.js
+++ webpack.config.js
@@ -36,8 +36,12 @@ module.exports = {
 	]
 	},
 	devServer: {
-		contentBase: './public',
-		writeToDisk: true,
+    static: {
+      directory: './public'
+    },
+		devMiddleware: {
+			writeToDisk: true
+		},
 		historyApiFallback: true
 	},
 	externals: {
