49159a83767ba36f0e450c775bc973cd4db68f15
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-02-07 09:33:42 -0300

    feat: ensure EmailValidation returns falsy if email is valid

--- src/validation/validators/email/email-validation.ts
+++ src/validation/validators/email/email-validation.ts
@@ -5,6 +5,7 @@ export class EmailValidation implements fieldValidation {
 	constructor (readonly field: string) {}
 
 	validate (value: string): Error {
-		return new InvalidFieldError();
+		const emailRegex = /^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/i;
+		return emailRegex.test(value) ? null : new InvalidFieldError();
 	}
 }
