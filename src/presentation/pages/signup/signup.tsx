29452a051754474225d4ee326d4916ed555a14e0
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-02-20 15:30:33 -0300

    feat: ensure SignUp shows spinner on submit

--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -37,11 +37,20 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
 		state.passwordConfirmation
 	]);
 
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>): Promise<void> => {
+		event.preventDefault();
+		setState({
+			...state,
+			isLoading: true
+		});
+	};
+
 	return (
 		<div className={Styles.signup}>
 			<LoginHeader />
 			<Context.Provider value={ { state, setState } }>
-				<form className={Styles.form}>
+				{/* eslint-disable-next-line @typescript-eslint/no-misused-promises */}
+				<form className={Styles.form} onSubmit={handleSubmit}>
 					<h2>Criar Conta</h2>
 					<Input type='text' name='name' placeholder='Digite seu nome'/>
 					<Input type='email' name='email' placeholder='Digite seu e-mail'/>
