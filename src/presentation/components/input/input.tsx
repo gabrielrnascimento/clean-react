e2aa27583ecb90ab6f77f205d290410bc2ca7d72
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-02-21 20:13:15 -0300

    refactor: add input animations

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from 'react';
+import React, { useContext, useRef } from 'react';
 import Styles from './input-styles.scss';
 import Context from '@/presentation/contexts/form/form-context';
 import PropTypes from 'prop-types';
@@ -7,32 +7,35 @@ type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
 	const { state, setState } = useContext(Context);
+	const inputRef = useRef<HTMLInputElement>();
 	const error = state[`${props.name}Error`];
-	const enableInput = (event: React.FocusEvent<HTMLInputElement, Element>): void => {
-		event.target.readOnly = false;
-	};
-	const handleChange = (event: React.FocusEvent<HTMLInputElement, Element>): void => {
-		setState({
-			...state,
-			[event.target.name]: event.target.value
-		});
-	};
-	const getStatus = (): string => {
-		return error ? '🔴' : '🟢';
-	};
-	const getTitle = (): string => {
-		return error || 'Tudo certo!';
-	};
 	return (
 		<div className={Styles.inputWrap}>
-			<input {...props} data-testid={props.name} readOnly onFocus={enableInput} onChange={handleChange} />
-			<span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>{getStatus()}</span>
+			<input
+				{...props}
+				ref={inputRef}
+				placeholder=" "
+				data-testid={props.name}
+				readOnly
+				onFocus={e => { e.target.readOnly = false; }}
+				onChange={e => setState({ ...state, [e.target.name]: e.target.value })}
+			/>
+			<label onClick={() => { inputRef.current.focus(); }}>
+				{props.placeholder}
+			</label>
+			<span
+				data-testid={`${props.name}-status`}
+				title={error || 'Tudo certo!'}
+				className={Styles.status}>
+				{error ? '🔴' : '🟢'}
+			</span>
 		</div>
 	);
 };
 
 Input.propTypes = {
-	name: PropTypes.string
+	name: PropTypes.string,
+	placeholder: PropTypes.string
 };
 
 export default Input;
