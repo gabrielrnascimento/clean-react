5510d938b96763fc60e103f72ca4dc1b50b2192e
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-02-23 20:56:54 -0300

    test: ensure SignUp loads with correct state

---
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -0,0 +1,21 @@
+import * as FormHelper from '../support/form-helper';
+import faker from 'faker';
+
+describe('Login', () => {
+	beforeEach(() => {
+		cy.visit('signup');
+	});
+
+	it('Should load with correct initial state', () => {
+		cy.getByTestId('name').should('have.attr', 'readonly');
+		FormHelper.testInputStatus('name', 'Campo obrigatório');
+		cy.getByTestId('email').should('have.attr', 'readonly');
+		FormHelper.testInputStatus('email', 'Campo obrigatório');
+		cy.getByTestId('password').should('have.attr', 'readonly');
+		FormHelper.testInputStatus('password', 'Campo obrigatório');
+		cy.getByTestId('passwordConfirmation').should('have.attr', 'readonly');
+		FormHelper.testInputStatus('passwordConfirmation', 'Campo obrigatório');
+		cy.getByTestId('submit').should('have.attr', 'disabled');
+		cy.getByTestId('error-wrap').should('not.have.descendants');
+	});
+});
