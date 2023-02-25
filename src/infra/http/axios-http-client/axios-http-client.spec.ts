ccd697cb92046ce3c4f99f6ad14e3f1c1d41ee29
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-02-25 20:38:02 -0300

    test: ensure AxiosHttpClient calls axios.get with correct values

--- src/infra/http/axios-http-client/axios-http-client.spec.ts
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -1,6 +1,6 @@
 import { AxiosHttpClient } from './axios-http-client';
 import { mockAxios, mockHttpResponse } from '@/infra/test';
-import { mockPostRequest } from '@/data/test';
+import { mockGetRequest, mockPostRequest } from '@/data/test';
 import type axios from 'axios';
 
 jest.mock('axios');
@@ -21,26 +21,35 @@ const makeSut = (): SutTypes => {
 
 describe('AxiosHttpClient', () => {
 	describe('post', () => {
-		test('Should call axios with correct values', async () => {
+		test('Should call axios.post with correct values', async () => {
 			const request = mockPostRequest();
 			const { sut, mockedAxios } = makeSut();
 			await sut.post(request);
 			expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body);
 		});
 
-		test('Should return the correct status code and body', () => {
+		test('Should return correct response on axios.post', () => {
 			const { sut, mockedAxios } = makeSut();
 			const promise = sut.post(mockPostRequest());
 			expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
 		});
 
-		test('Should return the correct status code and body on failure', () => {
+		test('Should return correct error on axios.post', () => {
 			const { sut, mockedAxios } = makeSut();
 			mockedAxios.post.mockRejectedValueOnce({
 				response: mockHttpResponse()
 			});
 			const promise = sut.post(mockPostRequest());
 			expect(promise).toEqual(mockedAxios.post.mock.results[0].value);
+		});
+	});
+
+	describe('get', () => {
+		test('Should call axios.get with correct values', async () => {
+			const request = mockGetRequest();
+			const { sut, mockedAxios } = makeSut();
+			await sut.get(request);
+			expect(mockedAxios.get).toHaveBeenCalledWith(request.url);
 		});
 	});
 });
