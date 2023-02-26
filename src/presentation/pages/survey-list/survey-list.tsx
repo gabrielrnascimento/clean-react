c3d560affda35ca7b22c7557a735e84373852911
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-02-26 01:10:08 -0300

    refactor: move Header to its own file

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
 import Styles from './survey-list-styles.scss';
-import { Footer, Logo } from '@/presentation/components';
+import { Footer } from '@/presentation/components';
+import Header from '@/presentation/components/header/header';
 
 const SurveyList: React.FC = () => {
 	return (
 		<div className={Styles.surveyListWrap}>
-			<header className={Styles.headerWrap}>
-				<div className={Styles.headerContent}>
-					<Logo />
-					<div className={Styles.logoutWrap}>
-						<span>Gabriel</span>
-						<a href="#">sair</a>
-					</div>
-				</div>
-			</header>
+			<Header />
 			<div className={Styles.contentWrap}>
 				<h2>Enquetes</h2>
 				<ul>
