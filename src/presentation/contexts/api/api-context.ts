9de50a4b34fb02a7b0a66a553a91426d19391b87
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-02-28 20:50:22 -0300

    feat: add getCurrentAccount to ApiContext

--- src/presentation/contexts/api/api-context.ts
+++ src/presentation/contexts/api/api-context.ts
@@ -3,6 +3,7 @@ import { type AccountModel } from '@/domain/models';
 
 type Props = {
 	setCurrentAccount?: (account: AccountModel) => void
+	getCurrentAccount?: () => AccountModel
 };
 
 export default createContext<Props>(null);
