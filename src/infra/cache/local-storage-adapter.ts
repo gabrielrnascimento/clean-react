981203b5158cfc6b31918d53fdf4b84f1a03c8b8
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-02-28 21:09:15 -0300

    feat: ensure LocalStorageAdapter calls localStorage.getItem with correct value

--- src/infra/cache/local-storage-adapter.ts
+++ src/infra/cache/local-storage-adapter.ts
@@ -1,7 +1,11 @@
-import { type SetStorage } from '@/data/protocols/cache/set-storage';
+import { type getStorage, type SetStorage } from '@/data/protocols/cache';
 
-export class LocalStorageAdapter implements SetStorage {
+export class LocalStorageAdapter implements SetStorage, getStorage {
 	set (key: string, value: object): void {
 		localStorage.setItem(key, JSON.stringify(value));
+	}
+
+	get (key: string): any {
+		return JSON.parse(localStorage.getItem(key));
 	}
 }
