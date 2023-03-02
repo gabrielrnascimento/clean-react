74334ab8f77faaa07077be4b96488f4ee8a5dced
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-03-02 08:03:35 -0300

    refactor: move survey list items to components

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Styles from './survey-list-styles.scss';
-import { Footer, Icon, IconName } from '@/presentation/components';
+import { Footer } from '@/presentation/components';
 import Header from '@/presentation/components/header/header';
 
 const SurveyList: React.FC = () => {
@@ -10,21 +10,7 @@ const SurveyList: React.FC = () => {
 			<div className={Styles.contentWrap}>
 				<h2>Enquetes</h2>
 				<ul>
-					<li>
-						<div className={Styles.surveyContent}>
-							<Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
-							<time>
-								<span className={Styles.day}>22</span>
-								<span className={Styles.month}>03</span>
-								<span className={Styles.year}>2020</span>
-							</time>
-							<p>Qual é seu framework web favorito?</p>
-						</div>
-						<footer>Ver Resultado</footer>
-					</li>
-					<li></li>
-					<li></li>
-					<li></li>
+
 				</ul>
 			</div>
 			<Footer />
