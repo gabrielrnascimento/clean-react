c6c5744e795b11cb19f2de5dbfc0a308681cd8e4
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-03-12 17:14:39 -0300

    refactor: remove unused code

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,11 +1,7 @@
-import { UnexpectedError } from '@/domain/errors';
 import { type AccountModel } from '@/domain/models';
 import { makeLocalStorageAdapter } from '../factories/cache';
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
-	if (!account?.accessToken) {
-		throw new UnexpectedError();
-	}
 	makeLocalStorageAdapter().set('account', account);
 };
 
