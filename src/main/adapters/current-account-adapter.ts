287209ad056cd7b4be5a1d4234aa4b86fe8515ca
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-03-12 10:17:31 -0300

    feat: add AuthorizeHttpGetClientDecorator factory

--- src/main/adapters/current-account-adapter.ts
+++ src/main/adapters/current-account-adapter.ts
@@ -1,6 +1,6 @@
 import { UnexpectedError } from '@/domain/errors';
 import { type AccountModel } from '@/domain/models';
-import { makeLocalStorageAdapter } from '../factories/cache/local-storage-adapter-factory';
+import { makeLocalStorageAdapter } from '../factories/cache';
 
 export const setCurrentAccountAdapter = (account: AccountModel): void => {
 	if (!account?.accessToken) {
