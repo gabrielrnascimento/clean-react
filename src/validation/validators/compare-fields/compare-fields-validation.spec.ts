4b62d99a2dc06fb8b0d38a33d2bb670a20599869
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-03-23 21:02:54 -0300

    test: change test to ensure values are different

--- src/validation/validators/compare-fields/compare-fields-validation.spec.ts
+++ src/validation/validators/compare-fields/compare-fields-validation.spec.ts
@@ -22,8 +22,8 @@ describe('CompareFieldsValidation', () => {
 		const fieldToCompare = secondField;
 		const sut = makeSut(field, fieldToCompare);
 		const error = sut.validate({
-			[field]: faker.random.words(3),
-			[fieldToCompare]: faker.random.words(4)
+			[field]: 'any_value',
+			[fieldToCompare]: 'other_value'
 		});
 		expect(error).toEqual(new InvalidFieldError());
 	});
