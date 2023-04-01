973de5559c715ab1b1dcbc38e9aac7957d10ca2b
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-04-01 11:11:23 -0300

    feat: ensure SurveyResult presents correct initial state

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,14 +1,20 @@
 import Styles from './survey-result-styles.scss';
-import { Header, Footer, Loading, Calendar } from '@/presentation/components';
+import { type LoadSurveyResult } from '@/domain/usecases';
+import { Header, Footer, Loading, Calendar, Error } from '@/presentation/components';
 import FlipMove from 'react-flip-move';
-import React from 'react';
+import React, { useState } from 'react';
 
 const SurveyResult: React.FC = () => {
+	const [state] = useState({
+		isLoading: false,
+		error: '',
+		surveyResult: null as LoadSurveyResult.Model
+	});
 	return (
 		<div className={Styles.surveyResultWrap}>
 			<Header />
-			<div className={Styles.contentWrap}>
-				{true &&
+			<div data-testid="survey-result" className={Styles.contentWrap}>
+				{state.surveyResult &&
 					<>
 						<hgroup>
 							<Calendar date={new Date()} className={Styles.calendarWrap} />
@@ -34,7 +40,9 @@ const SurveyResult: React.FC = () => {
 						<button>Voltar</button>
 					</>
 				}
-				{ false && <Loading /> }
+				{ state.isLoading && <Loading /> }
+				{/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
+				{ state.error && <Error error={state.error} reload={() => {}} /> }
 			</div>
 			<Footer />
 		</div>
