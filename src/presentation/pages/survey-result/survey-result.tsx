48f6a85347daa2f068c65f3e6e85eb7d1ca3f84d
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-04-02 17:17:35 -0300

    feat: ensure SurveyResult goes back to SurveyList on back button click

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -4,6 +4,7 @@ import { Header, Footer, Loading, Calendar, Error } from '@/presentation/compone
 import { useErrorHandler } from '@/presentation/hooks';
 import FlipMove from 'react-flip-move';
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router-dom';
 
 type Props = {
 	loadSurveyResult: LoadSurveyResult
@@ -22,6 +23,7 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
 	});
 
 	const reload = (): void => { setState(old => ({ isLoading: false, surveyResult: null, error: '', reload: !old.reload })); };
+	const { goBack } = useHistory();
 
 	useEffect(() => {
 		loadSurveyResult.load()
@@ -48,7 +50,7 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
 								</li>
 							)}
 						</FlipMove >
-						<button>Voltar</button>
+						<button data-testid="back-button" onClick={goBack} >Voltar</button>
 					</>
 				}
 				{ state.isLoading && <Loading /> }
