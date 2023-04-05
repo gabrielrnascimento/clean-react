6910be8a3b2075931293a71de6ccd54e5535f0b4
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-04-04 21:12:52 -0300

    feat: ensure SurveyResult calls SaveSurveyResult on non active answer click

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,15 +1,16 @@
-import { type LoadSurveyResult } from '@/domain/usecases';
+import { type SaveSurveyResult, type LoadSurveyResult } from '@/domain/usecases';
 import { Header, Footer, Loading, Error } from '@/presentation/components';
 import { useErrorHandler } from '@/presentation/hooks';
-import { SurveyResultData } from '@/presentation/pages/survey-result/components';
+import { SurveyResultContext, SurveyResultData } from '@/presentation/pages/survey-result/components';
 import Styles from './survey-result-styles.scss';
 import React, { useEffect, useState } from 'react';
 
 type Props = {
 	loadSurveyResult: LoadSurveyResult
+	saveSurveyResult: SaveSurveyResult
 };
 
-const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
+const SurveyResult: React.FC<Props> = ({ loadSurveyResult, saveSurveyResult }: Props) => {
 	const handleError = useErrorHandler((error: Error) => {
 		setState(old => ({ ...old, surveyResult: null, error: error.message }));
 	});
@@ -20,6 +21,13 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
 		surveyResult: null as LoadSurveyResult.Model,
 		reload: false
 	});
+
+	const onAnswer = (answer: string): void => {
+		setState(old => ({ ...old, isLoading: true }));
+		saveSurveyResult.save({ answer })
+			.then()
+			.catch();
+	};
 
 	const reload = (): void => { setState(old => ({ isLoading: false, surveyResult: null, error: '', reload: !old.reload })); };
 
@@ -32,11 +40,13 @@ const SurveyResult: React.FC<Props> = ({ loadSurveyResult }: Props) => {
 	return (
 		<div className={Styles.surveyResultWrap}>
 			<Header />
-			<div data-testid="survey-result" className={Styles.contentWrap}>
-				{state.surveyResult && <SurveyResultData surveyResult={state.surveyResult } /> }
-				{ state.isLoading && <Loading /> }
-				{ state.error && <Error error={state.error} reload={reload} /> }
-			</div>
+			<SurveyResultContext.Provider value={{ onAnswer }}>
+				<div data-testid="survey-result" className={Styles.contentWrap}>
+					{state.surveyResult && <SurveyResultData surveyResult={state.surveyResult } /> }
+					{ state.isLoading && <Loading /> }
+					{ state.error && <Error error={state.error} reload={reload} /> }
+				</div>
+			</SurveyResultContext.Provider>
 			<Footer />
 		</div>
 	);
