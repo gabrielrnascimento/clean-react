c97150117c82cfa74e1e93dac286be7dcf8d5d4a
Gabriel Nascimento <gabriel.nascimento@hurb.com>
2023-04-09 11:53:29 -0300

    test: ensure SurveyResult logouts on AccessDeniedError

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -65,18 +65,26 @@ describe('SurveyResult', () => {
 	});
 	describe('save', () => {
 		const mockUnexpectedError = (): void => { Http.mockServerError(path, 'PUT'); };
+		const mockAccessDeniedError = (): void => { Http.mockForbiddenError(path, 'PUT'); };
 
 		beforeEach(() => {
 			cy.fixture('account').then(account => {
 				Helper.setLocalStorageItem('account', account);
 			});
 			mockLoadSuccess();
+			cy.visit('/surveys/any_id');
 		});
 
 		it('Should present error on UnexpectedError', () => {
 			mockUnexpectedError();
 			cy.get('li:nth-child(2)').click();
 			cy.getByTestId('error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve.');
+		});
+
+		it('Should logout on AccessDeniedError', () => {
+			mockAccessDeniedError();
+			cy.get('li:nth-child(2)').click();
+			Helper.testUrl('/login');
 		});
 	});
 });
