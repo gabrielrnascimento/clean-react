78b37c9c7e66df9a84b9fbdba30ca15e7cf91b91
Gabriel Nascimento <gabrielnascimento.grn@gmail.com>
2023-04-12 20:02:46 -0300

    feat: reset loginState on page load

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { Link, useHistory } from 'react-router-dom';
-import { useRecoilState, useRecoilValue } from 'recoil';
+import { useRecoilState, useRecoilValue, useResetRecoilState } from 'recoil';
 import { type Authentication } from '@/domain/usecases';
 import { Footer, LoginHeader, currentAccountState } from '@/presentation/components';
 import { type Validation } from '@/presentation/protocols/validation';
@@ -13,10 +13,12 @@ type Props = {
 };
 
 const Login: React.FC<Props> = ({ validation, authentication }: Props) => {
+	const resetLoginState = useResetRecoilState(loginState);
 	const { setCurrentAccount } = useRecoilValue(currentAccountState);
 	const history = useHistory();
 	const [state, setState] = useRecoilState(loginState);
 
+	useEffect(() => { resetLoginState(); }, []);
 	useEffect(() => { validate('email'); }, [state.email]);
 	useEffect(() => { validate('password'); }, [state.password]);
 
